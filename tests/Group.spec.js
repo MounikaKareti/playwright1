a1de75d11b9fb58d88871aafd595259f10d6c18c
MounikaKareti <mounikakareti2001@gmail.com>
2024-11-05 15:14:22 +0530

    Initial commit with playwright

---
+++ tests/Group.spec.js
@@ -0,0 +1,9 @@
+import{test,expect} from '@playwright/test'
+test('test1',async({page})=>{
+    console.log("test1...")
+
+})
+test('test2',async({page})=>{
+    console.log("test2...")
+
+})
---
+++ tests/login.spec.js
@@ -0,0 +1,6 @@
+import{test,expect} from '@playwright/test'
+test("Login",async({page})=>{
+    await page.goto('https://testautomationpractice.blogspot.com/')
+    await page.locator('#name').fill("mounika")
+    await page.locator('#email').fill("mounika@gmail.com")
+})
